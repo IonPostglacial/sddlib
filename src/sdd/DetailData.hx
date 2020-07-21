159f782734a98065e4fedcdc5bd59503fc49cad9
Nicolas Galipot <nicolas.galipot@enablon.com>
2020-07-21 22:41:28 +0200

    Add conversion from DetailData to SDD Representation.

--- src/sdd/DetailData.hx
+++ src/sdd/DetailData.hx
@@ -18,14 +18,16 @@ class DetailData {
 	public var fasc:Null<Int>;
 	public var page:Null<Int>;
 	public var detail:String;
+	public var fields:Array<Field>;
 	public var extra:Dynamic;
 
-	function new(name, nameCN, fasc, page, detail) {
+	function new(name, nameCN, fasc, page, detail, fields) {
 		this.name = name;
 		this.nameCN = nameCN;
 		this.fasc = fasc;
 		this.page = page;
 		this.detail = detail;
+		this.fields = fields;
 		this.extra = {};
 	}
 
@@ -51,6 +53,20 @@ class DetailData {
 		return desc;
 	}
 
+	public function toRepresentation():Representation {
+        return {
+			label: name + if (nameCN != null) ' // $nameCN' else "",
+			detail: "" +
+				fields.map(function(field) {
+					final value = Reflect.field(if (field.std) this else this.extra, field.id);
+					if (value == null || value == "") return "";
+					return '${field.label}: $value<br><br>';
+				}).join("") +
+				if (fasc != null) 'Flore Madagascar et Comores<br>fasc ${fasc}<br>page ${page}<br><br>' else "" +
+				detail,
+		};
+	}
+
 	public static function fromRepresentation(representation:Representation, extraFields:Array<Field>):DetailData {
 		final names = representation.label.split(" // ");
 		final name = names[0], nameCN = names[1];
@@ -64,7 +80,7 @@ class DetailData {
 			page = Std.parseInt(floreRe.matched(2));
 		}
 		var detail = floreRe.replace(removeFromDescription(representation.detail, fields.map(field -> field.label)), "");
-		final data = new DetailData(name, nameCN, fasc, page, detail);
+		final data = new DetailData(name, nameCN, fasc, page, detail, extraFields);
 
 		for (field in fields) {
 			Reflect.setField(if (field.std) data else data.extra, field.id, findInDescription(representation.detail, field.label));
